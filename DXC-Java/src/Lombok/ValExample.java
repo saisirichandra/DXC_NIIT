b2a91371354875e810e85ce8107961213a942506
saisirichandra <sirichandrapodugu@gmail.com>
2021-04-16 11:33:29 +0530

    Lombok

---
+++ DXC-Java/src/Lombok/ValExample.java
@@ -0,0 +1,29 @@
+package Lombok;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import lombok.val;
+
+public class ValExample {
+ public String example() {
+   val example = new ArrayList<String>();
+   example.add("Hello, World!");
+   val foo = example.get(0);
+   return foo.toLowerCase();
+ }
+ 
+ public void example2() {
+   val map = new HashMap<Integer, String>();
+   map.put(0, "zero");
+   map.put(5, "five");
+   for (val entry : map.entrySet()) {
+     System.out.printf("%d: %s\n", entry.getKey(), entry.getValue());
+   }
+ }
+  public static void main(String[] args) {
+	ValExample obj =new ValExample();
+	String foo=obj.example();
+	System.out.println(foo);
+	obj.example2();
+}
+}
